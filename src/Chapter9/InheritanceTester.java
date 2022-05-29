69971c4c909e60f8dfbd76dc7958126ed1e706f8
WalidFawzi <w.elbastawisi@diyarme.com>
2022-05-29 21:48:01 +0300

    inheritance from multiple classes

--- src/Chapter9/InheritanceTester.java
+++ src/Chapter9/InheritanceTester.java
@@ -3,6 +3,10 @@ package Chapter9;
 public class InheritanceTester {
     public static void main(String args[]){
         Employee employee = new Employee();
+        Rectangle rectangle = new Rectangle();
+        rectangle.print();
+        Square square = new Square();
+        square.print("Square");
 
     }
 }
